7f8e2a77bbb04c3efc6294237adde620a53c7175
Igor Santos <igorfs042@gmail.com>
2019-06-11 12:32:01 -0300

    Conexão com o MongoDB

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://instagram:instagram@cluster0-w4zkv.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+});
 
 // app.get('/', (req, res) =>{
 //     return res.send(`Hello ${req.query.name}`);
