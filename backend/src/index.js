cd0651531023d1515419a2062fc9c4d7461bd9d6
Igor Santos <igorfs042@gmail.com>
2019-06-12 11:46:16 -0300

    Upload e Redimensionamento de Imagem

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 
 mongoose.connect('mongodb+srv://instagram:instagram@cluster0-w4zkv.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
 });
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 
 app.use(require('./routes.js'));
 
