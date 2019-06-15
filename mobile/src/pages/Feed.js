8322d99d3425752533aaf10c93501fca227bbb5f
Igor Santos <igorfs042@gmail.com>
2019-06-15 00:11:02 -0300

    Conexão com a api e estrutura do feed

--- mobile/src/pages/Feed.js
+++ mobile/src/pages/Feed.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
-import { View, Image, TouchableOpacity } from 'react-native';
+import { View, Text, Image, TouchableOpacity, FlatList } from 'react-native';
+import api from '../services/api.js';
 
 import camera from '../assets/camera.png';
+import more from '../assets/more.png';
+import like from '../assets/like.png';
+import comment from '../assets/comment.png';
+import send from '../assets/send.png';
 
 export default class Feed extends Component {
   
@@ -13,7 +18,60 @@ export default class Feed extends Component {
     ),
   });
 
+  state = {
+    feed: [],
+  };
+
+  async componentDidMount(){
+      // this.registerToSocket();
+
+      const response = await api.get('posts');
+
+      this.setState({ feed: response.data });
+  }
+
   render() {
-    return <View />;
+    return (
+      <View>
+        <FlatList 
+          data={this.state.feed}
+          keyExtractor={post => post._id}
+          renderItem={({ item }) => (
+            <View>
+              <View>
+                <View>
+                  <Text>{item.author}</Text>
+                  <Text>{item.place}</Text>
+                </View>
+
+                <Image source={more} />
+              </View>
+
+              <Image source={{ uri: `http://10.0.0.105:3333/${item.image}` }} />
+
+              <View>
+                <View>
+                  <TouchableOpacity>
+                    <Image source={like} />
+                  </TouchableOpacity>
+
+                  <TouchableOpacity>
+                    <Image source={comment} />
+                  </TouchableOpacity>
+
+                  <TouchableOpacity>
+                    <Image source={send} />
+                  </TouchableOpacity>
+                </View>
+
+                <Text>{item.likes} curtidas</Text>
+                <Text>{item.description}</Text>
+                <Text>{item.hashtags}</Text>
+              </View>
+            </View>
+          )}
+        />
+      </View>
+    );
   }
 }
